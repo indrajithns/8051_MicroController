0eb6a7919877825de0a99d503630b4150688bc61
indrajithns <indrajithgowda1998@gmail.com>
2025-02-13 23:19:19 +0530

    Add files via upload

---
+++ Project05 - Multiple Pattren LED Blinking/Program/main.c
@@ -0,0 +1,40 @@
+#include<reg51.h>
+
+void Delay(int time);
+
+void main()
+{
+	while(1)
+	{
+		char temp;
+		for(temp = 0; temp < 4; temp++)
+		{
+			P2 = 0xAA;
+			Delay(200);
+			
+			P2 = 0x55;
+			Delay(200);
+			
+			P2 = 0xCC;
+			Delay(200);
+			
+			P2 = 0x33;
+			Delay(200);
+			
+			P2 = 0xF0;
+			Delay(200);
+			
+			P2 = 0x0F;
+			Delay(200);
+		}
+	}
+}
+
+void Delay(int time)
+{
+	unsigned int x, y;
+	for(x = 0; x < time; x++)
+	{
+		for(y = 0; y < time; y++);
+	}
+}
